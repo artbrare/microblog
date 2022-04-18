1669af1668d457aadd0b122d57bdfa5996e2db68
unknown <artbrare@gmail.com>
2022-04-18 18:55:50 -0500

    chapter databases and started the login chapter

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from flask import flash, redirect, render_template
 from flask.helpers import url_for
-
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -22,10 +23,21 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        user = User.query.filter_by(username=form.username.data).first()
+        print(user)
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
+@app.route('/logut')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
